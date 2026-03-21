163ff1025b5595dad4b0d8263d88aa3fc76656cf
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-21 18:11:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -5,20 +5,20 @@ import os
 
 class Config((object)):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8656231605:AAGVZvUNt7VJiqq1r7MT_wbhCLO229ztU2I")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 34724970))
+    API_HASH = os.environ.get("f240eae7c60e8e30c17203ab0e052f7e")
     # Get these values from my.telegram.org
     # Banned Unwanted Members..
-    BANNED_USERS = {int(x) for x in os.environ.get("BANNED_USERS", "").split()}
+    BANNED_USERS = {int(x) for x in os.environ.get("BANNED_USERS", "7892805795").split()}
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # The download location for auth users.
 
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 7892805795
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
@@ -36,9 +36,9 @@ class Config((object)):
     # your telegram id
     OWNER_ID = int(os.environ.get("OWNER_ID", ""))
     # database session name, example: xurluploader
-    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "videouploader")
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
     PROCESS_TEXT = """
     Process: {}
     """
